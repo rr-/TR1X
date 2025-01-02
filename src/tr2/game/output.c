027bdafebb4db65886396546b79c46e051a28a3b
walkawayy <81546780+walkawayy@users.noreply.github.com>
2025-01-02 00:04:22 -0500

    tr2: add mac builds

--- src/tr2/game/output.c
+++ src/tr2/game/output.c
@@ -864,8 +864,8 @@ int32_t Output_GetObjectBounds(const BOUNDS_16 *const bounds)
         return 0;
     }
 
-    constexpr int32_t vtx_count = 8;
-    const XYZ_32 vtx[vtx_count] = {
+    const int32_t vtx_count = 8;
+    const XYZ_32 vtx[] = {
         { .x = bounds->min_x, .y = bounds->min_y, .z = bounds->min_z },
         { .x = bounds->max_x, .y = bounds->min_y, .z = bounds->min_z },
         { .x = bounds->max_x, .y = bounds->max_y, .z = bounds->min_z },
